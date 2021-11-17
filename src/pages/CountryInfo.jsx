590f543c32e8c085c4bd2f47580a7c753f0fac61
Jose Angarita <amjose09@gmail.com>
2021-11-17 15:53:57 -0500

    Pagina de información de un pais version desktop creada

--- src/pages/CountryInfo.jsx
+++ src/pages/CountryInfo.jsx
@@ -1,10 +1,8 @@
 import { nanoid } from 'nanoid'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import { useParams, Link, useHistory } from 'react-router-dom'
 import useDarkMode from '../context/useDark'
 import { useData } from '../context/useMainData'
-import useFetch from '../hooks/useFetch'
 
 const CountryInfo = ({ history }) => {
 
@@ -49,43 +47,53 @@ const Country = ({ data, handleBack, dark }) => {
     }, [data.capital, data.tld])
 
     return (
-        <div className="py-1 px-8">
+        <div className="py-1 px-8 lg:px-16">
             <Link to="/">
                 <button
                     onClick={handleBack}
-                    className={`pr-6 pl-6 py-2 rounded-sm shadow-xl bg-ca-dark text-ca-white text-sm`}
+                    className={`pr-6 pl-6 py-2 rounded-sm shadow-xl ${dark ? "bg-ca-dark text-ca-light" : "bg-ca-white text-ca-verydark"} transition1`}
                 >
                     <i className="fas fa-angle-left mr-2"></i>
                     <span>Back</span>
                 </button>
             </Link>
-            <article className="mt-10">
+            <article className="mt-10 transition1 lg:flex">
                 <img
                     src={svg}
                     alt={common}
+                    className="lg:h-96 lg:mr-36"
                 />
-                <h2 className={`mt-10 font-semibold text-3xl ${dark ? "text-ca-light" : "text-ca-darkest"}`}>{common}</h2>
-                <ul className="mt-6">
-                    <SpecificInfo title="Native name" info={comun} />
-                    <SpecificInfo title="Population" info={data.population} />
-                    <SpecificInfo title="Region" info={data.region} />
-                    <SpecificInfo title="Sub Region" info={data.subregion} />
-                    <SpecificInfo title="Capital" info={cap} />
-                </ul>
-                <ul className="mt-12">
-                    <SpecificInfo title={"Top Level Domain"} info={domain} />
-                    <SpecificInfo title={"Currency"} info={currencyName} />
-                    <SpecificInfo title={"Languages"} info={languages.join(", ")} />
-                </ul>
-                <ul className="mt-12">
-                    <h2 className={` text-lg ${dark ? "text-ca-light opacity-95" : "text-ca-verydark font-semibold"} opacity-95`}>Border countries:</h2>
-
-                    <div className="grid grid-cols-3 gap-3">
-                        {
-                            data.borders.map(e => <ButtonBorder alpha={e} key={nanoid()} />)
-                        }
+                {/* <img
+                    src={png}
+                    alt={common}
+                    className="hidden lg:block"
+                /> */}
+                <div>
+                    <h2 className={`mt-10 lg:mt-7 font-semibold text-3xl ${dark ? "text-ca-light" : "text-ca-darkest"}`}>{common}</h2>
+                    <div className="lg:flex items-baseline mt-6 lg:mt-4">
+                        <ul className="lg:mr-20">
+                            <SpecificInfo title="Native name" info={comun} />
+                            <SpecificInfo title="Population" info={data.population} />
+                            <SpecificInfo title="Region" info={data.region} />
+                            <SpecificInfo title="Sub Region" info={data.subregion} />
+                            <SpecificInfo title="Capital" info={cap} />
+                        </ul>
+                        <ul className="mt-12 lg:mt-0">
+                            <SpecificInfo title={"Top Level Domain"} info={domain} />
+                            <SpecificInfo title={"Currency"} info={currencyName} />
+                            <SpecificInfo title={"Languages"} info={languages.join(", ")} />
+                        </ul>
                     </div>
-                </ul>
+                    <ul className="mt-12 lg:flex items-center">
+                        <h2 className={`text-lg ${dark ? "text-ca-light opacity-95" : "text-ca-verydark font-semibold"} opacity-95`}>Border countries:</h2>
+
+                        <div className={`grid grid-cols-3 gap-3 lg:ml-4 ${data.borders.length>6&&"grid-cols-4"}`}>
+                            {
+                                data.borders.map(e => <ButtonBorder alpha={e} key={nanoid()} />)
+                            }
+                        </div>
+                    </ul>
+                </div>
             </article>
         </div>
     )
@@ -94,6 +102,8 @@ const Country = ({ data, handleBack, dark }) => {
 
 const ButtonBorder = ({ alpha }) => {
 
+    const history = useHistory()
+
     const { dark } = useDarkMode()
 
     const { mainData } = useData()
@@ -101,7 +111,10 @@ const ButtonBorder = ({ alpha }) => {
     const { common } = myCountry.name
 
     return (
-        <button className={`text-sm px-3 py-2 rounded-sm ${dark ? "bg-ca-dark text-ca-light" : "bg-ca-white text-ca-verydark"}`}>
+        <button
+            className={`shadow-lg text-sm px-3 py-2 rounded-sm ${dark ? "bg-ca-dark text-ca-light" : "bg-ca-white text-ca-verydark"} transition1`}
+            onClick={() => { history.push(`/country/${common}`) }}
+        >
             {common}
         </button>
     )
